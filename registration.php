e8dce7f95e99984f2ff1600add1fb27ccd35bd32
Nikolas <nikolakisae@gmail.com>
2022-04-13 14:02:53 +0300

    Compatibiliy update 2.4.4

--- registration.php
+++ registration.php
@@ -5,11 +5,14 @@
  * @category     MageVision
  * @package      MageVision_AdminCategoryProductThumbnail
  * @author       MageVision Team
- * @copyright    Copyright (c) 2021 MageVision (http://www.magevision.com)
+ * @copyright    Copyright (c) 2022 MageVision (http://www.magevision.com)
  * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
     'MageVision_AdminCategoryProductThumbnail',
     __DIR__
 );
